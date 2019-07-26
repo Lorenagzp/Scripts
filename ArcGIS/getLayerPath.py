17eded902bf89ad5bd6a58a0f220aa4250243d6b
Lorena González Pérez <lorenagzp@gmail.com>
2019-07-26 08:59:25 -0700

    Erase tests from the ArcGIS folder
    Erase tests from the ArcGIS folder

--- ArcGIS/getLayerPath.py
+++
@@ -1,8 +0,0 @@
-from os.path import exists
-
-#Get layer to obtain absolute path
-layer = arcpy.GetParameterAsText(0)
-arcpy.AddMessage("Layer is: " + layer)
-desc = arcpy.Describe(layer)
-path = desc.path
-arcpy.SetParameterAsText(1, str(path))

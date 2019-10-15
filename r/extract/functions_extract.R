1d42d470c38d198026848d319c81e40565e462e3
Lorena González Pérez <lorenagzp@gmail.com>
2019-10-15 09:12:17 -0700

    add different summary function options

--- r/extract/functions_extract.R
+++ r/extract/functions_extract.R
@@ -8,11 +8,18 @@ require(raster) # For importing rasters and shapefile
 require(velox) # For fast raster processing
 #############################################################
 
-#### Function to choose a CSV file interactively.
+#### Function to choose a CSV file interactively, halts if the input files or folders dont exist.
 ##   accepts to set the default location to open in the search window.
 askCSV <- function (default){
+  #Interactive ask CSV file
   f <- choose.files(default = "", multi = FALSE, caption = "Select the input  *.CSV file", filters = matrix(c("CSV File","*.csv"),1,2, byrow= TRUE))
-  return(read.csv(f,stringsAsFactors=FALSE))
+  #Open data table CSV
+  inputList <- read.csv(f,stringsAsFactors=FALSE)
+  #check valid filenames 
+  if ((invalidInputFiles(inputList))) {
+    stop("The input list has some invalid input file name") #Stop script if there are invalid filenames in the list.
+  }
+  return(inputList)
 }
 
 #### Function to choose a shapefile file interactively.
@@ -30,6 +37,35 @@ askRaster <- function (default){
   return(f) # Return raster name 
 }
 
+# Check the first 3 columns of the input files list, i.e. the raster filenames, the zones shapefile names and output folder locations for extraction, in_mode ==1
+invalidInputFiles <- function(inputList){
+  unexistingFiles <- !apply(inputList[,c(1:3)],2,file.exists)
+  if (any(unexistingFiles)) {
+    print("The following files in list don't exist")
+    print(inputList[,1:3][unexistingFiles]) 
+    return(TRUE)
+  } else (return(FALSE)) # No invalid filenames
+}
+
+#######################################################################
+#### Statistics Functions removing NA values
+#######################################################################
+## Median removing NA
+median_na <- function(x) median(x, na.rm = TRUE)
+
+## Mean removing NA
+mean_na <- function(x) mean(x, na.rm = TRUE)
+
+## sd removing NA
+sd_na <- function(x) sd(x, na.rm = TRUE)
+
+## 1st quartile removing NA
+Q1_na <- function(x) quantile(x, prob=c(0.25), na.rm = TRUE)
+
+## 3rd quartile removing NA
+Q3_na <- function(x) quantile(x, prob=c(0.75), na.rm = TRUE)
+
+
 #######################################################################
 #### Functions to perform the zonal statistics on multiband rasters
 #### Libraries used:
@@ -37,7 +73,7 @@ askRaster <- function (default){
 #######################################################################
 
 #### Extract vales from images
-extractThis <- function(r_file,zones, outFolder, ID_field ="Name",fun=mean,buf=0){
+extractThis <- function(r_file,zones, outFolder, ID_field ="Name",func=median_na,buf=0){
   ##   Imports raster as stack to allow multiple bands, uses "velox" object to make raster operations faster
   r <- velox(stack(r_file))
   #Set output name like the raster (remove extension and path from), save in the output folder
@@ -48,9 +84,12 @@ extractThis <- function(r_file,zones, outFolder, ID_field ="Name",fun=mean,buf=0
     zones <- buffer(zones, width = buf, dissolve= FALSE)
   }
   ## With this we get a table with the values for all the plots and bands
-  table <- r$extract(zones, df=TRUE, fun= function(x) fun(x, na.rm = TRUE)) #Perform the extraction with the selected statistic function, remove NA values
-  ## Set ID names from the shapefile
+  table <- r$extract(zones, df=TRUE, fun= func) #Extract and summarize the function, remove NA values. Returns a dataframe
+  ## Set ID names from the shapefile IF field
   table$ID_sp <- zones@data[,ID_field] 
   #Save to disk
   write.csv(table,OutTableName, row.names = FALSE)
 }
+
+
+

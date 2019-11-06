928bbb7a2f0db2cf67a1f9f9e7b2245b56f21faa
Lorena González Pérez <lorenagzp@gmail.com>
2019-11-06 11:36:07 -0700

    small improvements of code

--- r/extract/extract_from_raster_based_on_polygon_zones.R
+++ r/extract/extract_from_raster_based_on_polygon_zones.R
@@ -1,5 +1,6 @@
 ###################################################################################
-#### Script to extract the values from multiband rasters (Tif, Tiff and BSQ) based on vector zones ####
+#### Script to extract the values from multiband rasters (Tif, Tiff and BSQ) based on vector zones 
+#### Summarize the extracted pixels to give one value per zone
 #### Recopilado por Lorena González,  Septiembre 2019                          ####
 ####___________________________________________________________________________####
 #### There are two options for the Inputs: 
@@ -25,19 +26,27 @@
 
 
 #### Get functions and libraries
-source("F:\\Dropbox\\Software\\Scripts\\r\\extract\\functions_extract.R") #Check that this file location
+## First set the working directory to the location of this script
+#setwd("C:\\")
+source("functions_extract.R") #Check that the function file is in the same Working directory
 #require(raster)
 
-## Ask user where to put the output tables
+#### Ask user where to put the output tables
 #outFolder <- choose.dir(caption = "Select folder to save output tables")
 outFolder <- ("C:\\temp\\r") # Set path fix
 
-#summarize using the selected function
-func <- "median" #mean #min #max
+#### summarize using the selected function ("na" stands for: remove NA values)
+# See the functions_extract.R
+func <- median_na ## Summarize by median, remove NA values
+#func <- mean_na ## Summarize by mean, remove NA values
+#func <- sd_na ## Summarize by standard deviation, remove NA values
+#func <- Q1_na ## Summarize by first quartile, remove NA values
+#func <- Q3_na ## Summarize by third quartile, remove NA values
 
-#Field in the shp table to use as identifier of the extracting features
+####Field in the shp table to use as identifier of the extracting features
 ID_field <- "Name"
 
+#### Start of script
 tryCatch({ ## Put it all inside a handle error function
   
     ## Shapefile
@@ -49,11 +58,12 @@ tryCatch({ ## Put it all inside a handle error function
       
       #Select the CSV file with the list of inputs
       inputList <- askCSV() # read list from file
+
       ####EXTRACT####
       # run for every item in te list
       for (i in 1:nrow(inputList)) {
-        print(paste("Processing... ",i))
-        extractThis(inputList[i,1],shapefile(inputList[i,2]), inputList[i,3], inputList[i,4],func,as.double(inputList[i,5])) ## r_file,zones, outFolder, ID_field, fun=mean, buf
+        print(paste("Processing list... ",i))
+        extractThis(inputList[i,1],shapefile(inputList[i,2]), inputList[i,3], inputList[i,4],func,as.double(inputList[i,5])) ## r_file,zones, outFolder, ID_field, func, buf
       }
       
     } else if (in_mode == 2 || in_mode == 3){ ## Enter inputs
@@ -76,12 +86,12 @@ tryCatch({ ## Put it all inside a handle error function
         ## Ask user for Raster folder
         rFolder <- choose.dir(caption = "Select folder that contains the rasters to extract")
         #list the rasters inside the folder. Return full path name
-        r_list <- list.files(path = rFolder, full.names= TRUE, pattern = "\\.tif$|\\.bsq$|\\.tiff$") # Select Tif and BSQ for example
+        r_list <- list.files(path = rFolder, full.names= TRUE, pattern = "\\.tif$|\\.bsq$|\\.tiff$") # Select Tif or BSQ for example
         ## Extract each raster
         for (r_file in r_list) {
           ####EXTRACT#### 
-          print(paste("Processing... ",r_file))
-          extractThis(r_file,zones, outFolder,ID_field,fun = func,buf)
+          print(paste("Processing raster in folder... ",r_file))
+          extractThis(r_file,zones, outFolder,ID_field,func,buf)
         }
       }
       
@@ -90,14 +100,14 @@ tryCatch({ ## Put it all inside a handle error function
         r_file <- askRaster() #Get th raster name
         print(paste("Processing... ",r_file))
         ## Run extraction and saves output, indicate statistic
-        extractThis(r_file,zones, outFolder,ID_field, fun = func,buf)
+        extractThis(r_file,zones, outFolder,ID_field,func,buf)
 
       }
     }
     print("Finish extracting")
   },
   #In case of halting error:
-  error = function(e){print(c("Se produjo un error: ",e$message))}#,
+  error = function(e){print(c("An error ocurred: ",e$message))}#,
   # In case of warnings:
   #warning = function(e){print(paste("Hay advertencias: ", e$message))}
 )

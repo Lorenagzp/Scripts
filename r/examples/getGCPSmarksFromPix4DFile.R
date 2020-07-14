088f492b8b68c179d163688d669e6f8448dbdbe7
Lorena González Pérez <lorenagzp@gmail.com>
2020-07-14 10:26:55 -0700

    Update getGCPSmarksFromPix4DFile.R
    comments

--- r/examples/getGCPSmarksFromPix4DFile.R
+++ r/examples/getGCPSmarksFromPix4DFile.R
@@ -4,7 +4,8 @@
 ##Load package to read XML
 library("XML")
 ## Select files from the project folders (that are in the WD), recursively
-setwd("C:/temp/important/Hibap_2017-2018_GCPmarks/RGB_HiBAP")
+#setwd("C:/temp/important/Hibap_2017-2018_GCPmarks/RGB_HiBAP")
+setwd(choose.dir())
 p4dFiles <- list.files(getwd(),pattern = "\\.p4d$",recursive = TRUE)
 
 ##Read info from each p4f filename
@@ -12,9 +13,14 @@ for (f in p4dFiles) {
   p4d <- xmlRoot(xmlParse(file = f)) #read text (it is an XML file)
   gcps <- p4d[["inputs"]][["gcps"]] #Get GCPS info
   imgs<- p4d[["inputs"]][["images"]] # Get the list of image info
+  
+  ## If you need the image names and coordinates list
+  #imgs_name<-  xpathApply(imgs,"//image/@path") # Get the list of image names
+  #imgs_coords<- xpathApply(imgs,"//image//gps") # Get the list of image coords
+  
   ## Save in the folder of the .p4d file
   outpath = dirname(f) #Set ouput folder
   outfilename = tools::file_path_sans_ext(basename(f))  #set output basename
   saveXML(imgs, file.path(outpath, paste0(outfilename,"_imageGCP.XML")), fsep=.Platform$file.sep) #Save img-gcp marks info
-  saveXML(imgs, file.path(outpath, paste0(outfilename,"_gcps.XML")), fsep=.Platform$file.sep) #Save gcps info
+  saveXML(gcps, file.path(outpath, paste0(outfilename,"_gcps.XML")), fsep=.Platform$file.sep) #Save gcps info
 }
